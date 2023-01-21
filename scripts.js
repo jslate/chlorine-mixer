ace81c8b8e90db82c63774ad4bb061bb3b2e787d
Jonathan Slate <jslate@epionhealth.com>
2023-01-21 18:52:37 -0500

    Remove stuff I didn't use

--- scripts.js
+++ scripts.js
@@ -1,15 +1,7 @@
 document.addEventListener('DOMContentLoaded', e => {
   const codeElement = document.getElementById('code');
   const jar = CodeJar(codeElement, hljs.highlightElement);
-  function getCode() {
-    var code = jar.toString();
-    var options = {
-      'presets': ['es2015']
-    };
-    return Babel.transform(code, options).code;
-  }
   // return; // TO_START_EDITING: remove the first two slashes at the beginning of this line
-
 
   function updateHandler() {
     document.getElementById('calculate').remove();
@@ -19,7 +11,7 @@ document.addEventListener('DOMContentLoaded', e => {
     button.class = 'calculate';
 
     document.getElementById('calculateContainer').prepend(button);
-    const scriptText = `document.getElementById('calculate').addEventListener('click', () => { ${getCode()}; document.getElementById('result').style.display = 'block'; });`;
+    const scriptText = `document.getElementById('calculate').addEventListener('click', () => { ${jar.toString()}; document.getElementById('result').style.display = 'block'; });`;
     const oldScript = document.getElementById('scriptContainer');
 
     if (oldScript) {
